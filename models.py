18d8145204811ca06ed0ec538272b214b5f01cef
mithunpranava <mithunpranava@gmail.com>
2025-06-08 16:18:04 +0530

    scalable todo webapp backend

---
+++ models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(120), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+
+    def to_dict(self):
+        return {"id": self.id, "task": self.task, "done": self.done}
